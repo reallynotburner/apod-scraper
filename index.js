986fa6b94f717723206283d4dedd3b17c4d53445
reallynotburner <reallynotburner@gmail.com>
2020-09-17 09:03:19 -0400

    updated comments, moved table name to .env file

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const fetch = require('isomorphic-fetch');
 const mySqlEndpoint = process.env.MYSQL_ENDPOINT;
 const mySqlUser = process.env.MYSQL_USER;
 const mySqlPassword = process.env.MYSQL_PASSWORD;
-const mySqlDatabase = process.env.MYSQL_DATABASE;
+const mySqlDatabaseName = process.env.MYSQL_DATABASE;
+const mySqlTableName = process.env.MYSQL_TABLE;
 const sqlConfig = {
   host: mySqlEndpoint,
   user: mySqlUser,
   password: mySqlPassword,
-  database: mySqlDatabase
+  database: mySqlDatabaseName
 };
 const apiKey = process.env.NASA_API_KEY;
 
@@ -28,7 +29,7 @@ async function scrapeApod(apiKey, offset = 1) {
   const con = await sqlConnectPromise(sqlConfig);
   if (!con) throw 'Bad MySQL Connection';
   
-  const recentDateSql = `SELECT DATE_FORMAT(date,\'%Y-%m-%d\') date from ApodApiRecords ORDER by id DESC LIMIT 1`;
+  const recentDateSql = `SELECT DATE_FORMAT(date,\'%Y-%m-%d\') date from ${mySqlTableName} ORDER by id DESC LIMIT 1`;
   const recentDateResult = await sqlQueryPromise(con, recentDateSql);
 
   if (recentDateResult.length > 0) {
@@ -50,6 +51,9 @@ async function scrapeApod(apiKey, offset = 1) {
   cursorMonth = cursorDate.getMonth();
   cursorDay = cursorDate.getDate();
 
+  // TODO: learn date comparison / manipulation patters.
+  // until now, I've only used Date.now() to see what the 
+  // process times look like in millis.
   if (
     cursorYear > stopYear ||
     cursorYear === stopYear && cursorMonth > stopMonth ||
@@ -70,8 +74,10 @@ async function scrapeApod(apiKey, offset = 1) {
       .then(r => r.json())
       .then(r => {
         if (r.code || r.msg) {
+          // TODO: get a better way to alternatively NOT call
+          // the sql.  Probably a good case for async/await pattern here.
           offset = offset + 1;
-          return `SELECT * from ApodApiRecords LIMIT 0`;
+          return `SELECT * from ${mySqlTableName} LIMIT 0`;
         } else if (
           r.hasOwnProperty('error') &&
           r.error.code === 'OVER_RATE_LIMIT'
@@ -85,7 +91,7 @@ async function scrapeApod(apiKey, offset = 1) {
           throw 'Ran out of Number of requests!';
         }
         offset = 1;
-        const sql = `INSERT INTO ApodApiRecords (date, title, media_type, url, hdurl, explanation, copyright) ` +
+        const sql = `INSERT INTO ${mySqlTableName} (date, title, media_type, url, hdurl, explanation, copyright) ` +
           `VALUES (${con.escape(r.date)},` +
           ` ${con.escape(r.title && r.title)},` +
           `  ${con.escape(r.media_type && r.media_type)},` +
